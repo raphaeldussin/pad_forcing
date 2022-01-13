c0625d792c33bbee51e1757f8f163f57e363f586
Raphael Dussin <Raphael.Dussin@noaa.gov>
2022-01-12 21:24:50 -0500

    finished parsing and concat

--- pad_forcing/padding.py
+++ pad_forcing/padding.py
@@ -1,17 +1,26 @@
+#!/usr/bin/env python
+
 import argparse
 import warnings
 from glob import glob
 import re
+import xarray as xr
+import os
+import cftime
 
 
-def find_file(possible_directories, varname, yearstart, yearend):
-    """ find the file for variable and time segment
+def find_file(possible_directories, varname, yearstart, yearend, exclude_pattern=None):
+    """find the file for a give varname spanning yearstart to yearend in possible_directories
 
     Args:
-        possible_directories ([type]): [description]
-        varname ([type]): [description]
-        yearstart ([type]): [description]
-        yearend ([type]): [description]
+        possible_directories (list of str): directories containing forcing files
+        varname (str): variable to look for in files
+        yearstart (int): first year of the file
+        yearend (int): last year of the file
+        exclude_pattern (list of str, optional): patterns in filenames to exclude. Defaults to None.
+
+    Returns:
+        str: filename
     """
 
     all_files = []
@@ -19,99 +28,200 @@ def find_file(possible_directories, varname, yearstart, yearend):
     # get all files for the time segment
     for d in possible_directories:
         all_files += glob(f"{d}/*{yearstart}*{yearend}*")
+        # if filename only have one year mentioned
+        if len(all_files) == 0:
+            all_files += glob(f"{d}/*{yearstart}*")
     # we should have at least one
     if len(all_files) == 0:
-        raise ValueError("no files found to pad this file")
+        exit(">>> ERROR: no files found to pad this file")
     # now look for variable name
-    print(all_files)
     for f in all_files:
         out = re.findall(f"{varname}[(_|.)]", f)
         if len(out) > 0:
             possible_files.append(f)
     # we should have found one and only one
     if len(possible_files) > 1:
-        print(possible_files)
-        raise ValueError("could not find single valid file")
+        if exclude_pattern is not None:
+            for tag in exclude_pattern:
+                for f in possible_files:
+                    if f.find(tag) != -1:
+                        possible_files.remove(f)
+            if len(possible_files) != 1:
+                exit(
+                    f">>> ERROR: cannot converge on a single file, current left with {len(possible_files)}. Consider changing exclude pattern"
+                )
+        else:
+            print(">>> found multiple files for padding", possible_files)
+            print(">>> consider excluding the undesired pattern(s)")
+            exit(">>> could not find single valid file")
 
     return possible_files[0]
 
 
-#def find_similar_file(possible_directories, fname, current_start, current_end, new_start, new_end):
-#    expected_name = fname.replace(current_start, new_start)
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
 
-
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    "-d",
-    "--dirs",
-    type=str,
-    nargs="+",
-    default="./",
-    help="Path(s) to input nc file(s)",
-)
-
-parser.add_argument("infile", metavar="INFILE", type=str, help="forcing file to pad")
-
-parser.add_argument(
-    "--first", action="store_true", help="this file is the first of the timeserie"
-)
-parser.add_argument(
-    "--last", action="store_true", help="this file is the last of the timeserie"
-)
-parser.add_argument("--var", type=str, help="variable to process")
-parser.add_argument("--year", type=str, help="override for year to process")
-
-args = vars(parser.parse_args())
-
-print(args)
-
-if args["first"] and args["last"]:
-    warnings.warn(
-        "first and last are allowed simultaneouly but make sure this is only in the case of having a single file"
+    parser.add_argument(
+        "-d",
+        "--dirs",
+        type=str,
+        nargs="+",
+        default="./",
+        help="Path(s) to input nc file(s)",
     )
 
-if args["year"] is None:
-    # find the year from the filename
-    # use regular expressions to get dates (have more than 4 digits)
-    dates_from_fname = re.findall("[0-9][0-9][0-9][0-9]+", args["infile"])
-    if len(dates_from_fname) == 2:
-        start_date, end_date = dates_from_fname
-    elif len(dates_from_fname) == 1:
-        start_date = dates_from_fname[0]
-        end_date = dates_from_fname[0]
-    else:
-        raise ValueError(
-            "year retrieval yield not enough or too many values, use --year to override"
+    parser.add_argument(
+        "infile", metavar="INFILE", type=str, help="forcing file to pad"
+    )
+
+    parser.add_argument(
+        "--first", action="store_true", help="this file is the first of the timeserie"
+    )
+    parser.add_argument(
+        "--last", action="store_true", help="this file is the last of the timeserie"
+    )
+    parser.add_argument("--var", type=str, help="variable to process")
+
+    parser.add_argument("--year", type=str, help="override for year to process")
+
+    parser.add_argument(
+        "--exclude", type=str, nargs="+", help="exclude some file patterns"
+    )
+
+    parser.add_argument(
+        "--time",
+        type=str,
+        default="time",
+        help="override for time variable name in file",
+    )
+
+    args = vars(parser.parse_args())
+
+    if args["first"] and args["last"]:
+        warnings.warn(
+            "first and last are allowed simultaneouly but make sure this is only in the case of having a single file"
         )
 
-    start_year = int(start_date[:4])
-    end_year = int(end_date[:4])
-else:
-    # this could be exapnded to handle different start and end year, if needed.
-    start_year = args["year"]
-    end_year = args["year"]
+    if args["year"] is None:
+        # find the year from the filename
+        # use regular expressions to get dates (have more than 4 digits)
+        dates_from_fname = re.findall("[0-9][0-9][0-9][0-9]+", args["infile"])
+        if len(dates_from_fname) == 2:
+            start_date, end_date = dates_from_fname
+        elif len(dates_from_fname) == 1:
+            start_date = dates_from_fname[0]
+            end_date = dates_from_fname[0]
+        else:
+            raise ValueError(
+                ">>> ERROR: year retrieval yield not enough or too many values, use --year to override"
+            )
 
-print(start_year, end_year)
+        start_year = int(start_date[:4])
+        end_year = int(end_date[:4])
+    else:
+        # this could be exapnded to handle different start and end year, if needed.
+        start_year = args["year"]
+        end_year = args["year"]
 
-years_per_file = end_year - start_year + 1
-previous_start_year = start_year - years_per_file
-previous_end_year = start_year - 1
+    print(f">>> current file spans the years {start_year} to {end_year}")
 
-next_start_year = end_year + 1
-next_end_year = end_year + years_per_file
+    years_per_file = end_year - start_year + 1
+    previous_start_year = start_year - years_per_file
+    previous_end_year = start_year - 1
 
-varname = args["var"]
+    next_start_year = end_year + 1
+    next_end_year = end_year + years_per_file
 
-if not args["first"]:
-    print(f">>> looking for a file for {varname} between {previous_start_year} and {previous_end_year}")
-    previous_file = find_file(args["dirs"], varname, previous_start_year, previous_end_year)
-    print(f">>> found {previous_file}")
+    varname = args["var"]
+    timevar = args["time"]
 
-if not args["last"]:
-    print(f">>> looking for a file for {varname} between {next_start_year} and {next_end_year}")
-    next_file = find_file(args["dirs"], varname, next_start_year, next_end_year)
-    print(f">>> found {next_file}")
+    # find the directory containing the input file
+    cdir = None
+    for d in args["dirs"]:
+        if os.path.exists(f"{d}/{args['infile']}"):
+            cdir = d
 
+    if cdir is None:
+        exit(">>> ERROR: forcing file not found in directories")
+
+    # open the file for current year and estimate frequency of data
+    current = xr.open_dataset(f"{cdir}/{args['infile']}", decode_times=False)
+    dt = current[timevar].diff(dim=timevar).isel({timevar: 0})
+
+    # inspect variables
+    all_vars = list(set(list(current.variables) + list(current.coords)))
+    possible_time_bounds = [timevar + "bnds", timevar + "_bounds"]
+    time_bounds = None
+    for var in possible_time_bounds:
+        if var in all_vars:
+            time_bounds = var
+
+    # pad at beginning of file with last record of previous file or doubling first record
+    if args["first"]:
+        # repeat the first record
+        previous = current.isel({timevar: 0})
+        previous[timevar] = previous[timevar] - dt
+        if time_bounds is not None:
+            previous[time_bounds] = previous[time_bounds] - dt
+    else:
+        print(
+            f">>> looking for a file for {varname} between {previous_start_year} and {previous_end_year}"
+        )
+        previous_file = find_file(
+            args["dirs"],
+            varname,
+            previous_start_year,
+            previous_end_year,
+            exclude_pattern=args["exclude"],
+        )
+        print(f">>> found {previous_file}")
+        previous = xr.open_dataset(previous_file, decode_times=False).isel(
+            {timevar: -1}
+        )
+
+    # pad at end of file with first record of last file or doubling last record
+    if args["last"]:
+        # repeat the last record
+        next = current.isel({timevar: -1})
+        next[timevar] = next[timevar] + dt
+        if time_bounds is not None:
+            next[time_bounds] = next[time_bounds] + dt
+    else:
+        print(
+            f">>> looking for a file for {varname} between {next_start_year} and {next_end_year}"
+        )
+        next_file = find_file(
+            args["dirs"],
+            varname,
+            next_start_year,
+            next_end_year,
+            exclude_pattern=args["exclude"],
+        )
+        print(f">>> found {next_file}")
+        next = xr.open_dataset(next_file, decode_times=False).isel({timevar: 0})
+
+    # concatenate records and force time to be first dimension
+    out = xr.concat([previous, current, next], dim=timevar)
+    out = out.transpose(*(timevar, ...))
+
+    # clean up some dimensions/attributes
+    for var in all_vars:
+        if not args["time"] in current[var].dims:
+            # remove time dimension from variables
+            # that did not have it in the first place
+            # by overriding with original variable
+            out[var] = current[var]
+            # remove _FillValue and coordinates from attributes
+            out[var].encoding.update({"_FillValue": None, "coordinates": None})
+
+    # remove fill value on time
+    out[timevar].encoding.update({"_FillValue": None})
+
+    # remove fill value on time bounds, if appropriate
+    if time_bounds is not None:
+        out[time_bounds].encoding.update({"_FillValue": None, "coordinates": None})
+
+    # write output file
+    fileout = args["infile"].replace(".nc", ".padded.nc")
+    out.to_netcdf(fileout, format="NETCDF4_CLASSIC", unlimited_dims=args["time"])
